be62e64eee80456b9ddb0ee574bd3a90ce5920ab
Cpaluszek <paluszek.clement@gmail.com>
2023-11-17 18:16:40 +0100

    .obj file extension check

--- inc/program_options.h
+++ inc/program_options.h
@@ -1,7 +1,10 @@
 #ifndef INC_PROGRAM_OPTIONS_H_
 #define INC_PROGRAM_OPTIONS_H_
 
+#define DESIRED_EXTENSION ".obj"
+
 #include <string>
+
 
 namespace program_options {
     void parse(int argc, char* argv[]);
--- src/program_options.cpp
+++ src/program_options.cpp
@@ -15,7 +15,10 @@ void program_options::parse(const int argc, char* argv[]) {
         if (!std::filesystem::exists(arg)) {
             throw std::runtime_error(arg + ": No such file or directory");
         }
-        // Todo: check for .obj extension
+        std::filesystem::path path(arg);
+        if (path.extension() != DESIRED_EXTENSION) {
+            throw std::runtime_error("File is not in the correct format");
+        }
         if (!objFile.empty()) {
             throw std::runtime_error("Cannot use multiple .obj files");
         }
