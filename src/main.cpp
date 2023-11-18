55b67ab98876948a63ca5a5774cfe8651b763852
Cpaluszek <paluszek.clement@gmail.com>
2023-11-18 18:54:54 +0100

    Setup and load texture file OK

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@
 
 #include "../inc/program_options.h"
 #include "../inc/math.h"
+#include "../inc/stb_image.h"
 #include "glfw3.h"
 
 // Todo: stb_image.h authorized?
@@ -47,6 +48,31 @@ int main(const int argc, char *argv[]) {
         delete window;
         return EXIT_FAILURE;
     }
+
+    // Todo: check for errors
+    // Texture
+    GLuint texture;
+    glGenTextures(1, &texture);
+    glBindTexture(GL_TEXTURE_2D, texture);
+    // set the texture parameters - wrapping and filtering
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    // load and generate the texture
+    int width, height, nbChannels;
+    unsigned char* data = stbi_load("./resources/textures/uvmap.jpeg", &width, &height, &nbChannels, 0);
+    if (data) {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);    
+        glGenerateMipmap(GL_TEXTURE_2D);
+        stbi_image_free(data);
+    } else {
+        std::cerr << "Failed to load texture" << std::endl;
+    }
+
+    // Bind texture
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, texture);
     
     while (!glfwWindowShouldClose(window->instance)) {
         input.processInput(Window::getDeltaTime());
