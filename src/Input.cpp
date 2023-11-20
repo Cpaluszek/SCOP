df2efc3516bed591e8b29a8e65b90f5352eb66d7
Cpaluszek <paluszek.clement@gmail.com>
2023-11-20 10:16:54 +0100

    add keybind info in terminal

--- src/Input.cpp
+++ src/Input.cpp
@@ -2,7 +2,22 @@
 #include "glfw3.h"
 
 Input::Input(Camera& camera, GLFWwindow* windowInstance)
-: camera(camera), window(windowInstance) {}
+: camera(camera), window(windowInstance) {
+    std::cout << "----- Keybinds: -----" << std::endl;
+    std::cout << " Object movements:" << std::endl;
+    std::cout << "  W: move up" << std::endl;
+    std::cout << "  A: move left" << std::endl;
+    std::cout << "  S: move down" << std::endl;
+    std::cout << "  D: move right" << std::endl;
+    std::cout << "  R: move forward" << std::endl;
+    std::cout << "  F: move backward" << std::endl;
+    std::cout << " Object Rotation:" << std::endl;
+    std::cout << "  TODO" << std::endl;
+    std::cout << " Polygon Mode:" << std::endl;
+    std::cout << "  P: switch between wireframe and fill" << std::endl;
+    std::cout << "---------------------" << std::endl;
+
+}
 
 void Input::processInput(Model& model, const float deltaTime) { 
     if (glfwGetKey(this->window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
