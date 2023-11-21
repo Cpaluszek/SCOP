1d86b8f6a98572e085c2dd6f9a58576552ae3666
Cpaluszek <paluszek.clement@gmail.com>
2023-11-21 19:49:43 +0100

    replace bracket acces by .at()

--- src/ObjParser.cpp
+++ src/ObjParser.cpp
@@ -2,6 +2,7 @@
 #include <random>
 
 // Todo: in face parsing test for invalid index access
+// throw std::out_of_range
 
 ObjParser::ObjParser() {
     // Setup random distribution for colors
@@ -18,7 +19,7 @@ void ObjParser::parseObjFile(std::ifstream& file) {
         if (line.empty()) continue ;
 
         VecString lineSplit = utils::splitString(line, ' ');
-        if (lineSplit.size() == 1 || lineSplit[0] == COMMENT_KEYWORD) continue ;
+        if (lineSplit.size() == 1 || lineSplit.at(0) == COMMENT_KEYWORD) continue ;
         
         this->parseLine(lineSplit, line);
     }
@@ -26,12 +27,12 @@ void ObjParser::parseObjFile(std::ifstream& file) {
 
 void ObjParser::parseLine(const VecString& lineSplit, const std::string& line) {
     // Note: maybe use map with functions to iterate
-    if (lineSplit[0] == VERTEX_KEYWORD) {
+    if (lineSplit.at(0) == VERTEX_KEYWORD) {
         if (finalVertices.size() != 0) {
             throw std::runtime_error("Vertex to need be listed before faces");
         }
         this->parseVertex(lineSplit, line);
-    } else if (lineSplit[0] == FACE_KEYWORD) {
+    } else if (lineSplit.at(0) == FACE_KEYWORD) {
         if (parsedVertices.size() == 0) {
             throw std::runtime_error("Vertex need be listed before faces");
         } 
@@ -57,23 +58,23 @@ void ObjParser::parseLine(const VecString& lineSplit, const std::string& line) {
                 break;
         }
     }
-    else if (lineSplit[0] == VERTEX_NORMALS_KEYWORD) {
+    else if (lineSplit.at(0) == VERTEX_NORMALS_KEYWORD) {
         this->parseVertexNormal(lineSplit, line);
-    } else if (lineSplit[0] == SMOOTH_SHADING_KEYWORD) {
+    } else if (lineSplit.at(0) == SMOOTH_SHADING_KEYWORD) {
         this->parseSmoothShading(lineSplit);
-    } else if (lineSplit[0] == TEXT_COORDS_KEYWORD) {
+    } else if (lineSplit.at(0) == TEXT_COORDS_KEYWORD) {
         this->parseVertexTextureCoords(lineSplit, line);
-    } else if (lineSplit[0] == MAT_FILE_KEYWORD) {
+    } else if (lineSplit.at(0) == MAT_FILE_KEYWORD) {
         std::cerr << "Parsing: '" << line << "' is not implemented yet" << std::endl;
-    } else if (lineSplit[0] == MAT_NAME_KEYWORD) {
+    } else if (lineSplit.at(0) == MAT_NAME_KEYWORD) {
         std::cerr << "Parsing: '" << line << "' is not implemented yet" << std::endl;
-    } else if (lineSplit[0] == OBJ_NAME_KEYWORD) {
+    } else if (lineSplit.at(0) == OBJ_NAME_KEYWORD) {
         std::cerr << "Parsing: '" << line << "' is not implemented yet" << std::endl;
-    } else if (lineSplit[0] == GROUP_NAME_KEYWORD) {
+    } else if (lineSplit.at(0) == GROUP_NAME_KEYWORD) {
         std::cerr << "Parsing: '" << line << "' is not implemented yet" << std::endl;
-    } else if (lineSplit[0] == LINE_KEYWORD) {
+    } else if (lineSplit.at(0) == LINE_KEYWORD) {
         std::cerr << "This project does not support line elements" << std::endl;
-    } else if (lineSplit[0] == PARAM_SPACE_VERTICES_KEYWORD) {
+    } else if (lineSplit.at(0) == PARAM_SPACE_VERTICES_KEYWORD) {
         std::cerr << "This project does not support parameter space vertices" << std::endl;
     } else {
         std::cerr << "Parsing: '" << line << "' unknown keyword" << std::endl;
@@ -88,9 +89,9 @@ void ObjParser::parseVertex(
     }
 
     try {
-        GLfloat x = std::stof(lineSplit[1]);
-        GLfloat y = std::stof(lineSplit[2]);
-        GLfloat z = std::stof(lineSplit[3]);
+        GLfloat x = std::stof(lineSplit.at(1));
+        GLfloat y = std::stof(lineSplit.at(2));
+        GLfloat z = std::stof(lineSplit.at(3));
         Vertex vertex {
             Vec3f(x, y, z),
             Vec3f(),
@@ -114,9 +115,9 @@ void ObjParser::parseVertexNormal(
     }
 
     try {
-        GLfloat x = std::stof(lineSplit[1]);
-        GLfloat y = std::stof(lineSplit[2]);
-        GLfloat z = std::stof(lineSplit[3]);
+        GLfloat x = std::stof(lineSplit.at(1));
+        GLfloat y = std::stof(lineSplit.at(2));
+        GLfloat z = std::stof(lineSplit.at(3));
         Vec3f normal(x, y, z);
         this->normals.push_back(Vec3f::normalize(normal));
     } catch (const std::invalid_argument& e) {
@@ -134,8 +135,8 @@ void ObjParser::parseVertexTextureCoords(
     }
 
     try {
-        GLfloat u = std::stof(lineSplit[1]);
-        GLfloat v = lineSplit.size() > 2 ? std::stof(lineSplit[2]) : 0.0f;
+        GLfloat u = std::stof(lineSplit.at(1));
+        GLfloat v = lineSplit.size() > 2 ? std::stof(lineSplit.at(2)) : 0.0f;
         if (u < 0.0f || u > 1.0f || v < 0.0f || v > 1.0f) {
             throw std::runtime_error("Texture coords should in range [0.0f, 1.0f]");
         }
@@ -153,12 +154,12 @@ void ObjParser::parseFace(
              const std::string& line) {
     try {
         for (size_t i = 1; i < lineSplit.size(); i++) {
-            size_t slashPos = lineSplit[i].find('/');
+            size_t slashPos = lineSplit.at(i).find('/');
             if (slashPos != std::string::npos) {
                 throw std::runtime_error("Inconsistent face format");
             }
 
-            int index = std::stoi(lineSplit[i]);
+            int index = std::stoi(lineSplit.at(i));
             // Todo: check for neg or overflow
             if (i == 4) {
                 auto a = *(this->finalVertices.rbegin() + 3 - 1);
@@ -168,7 +169,7 @@ void ObjParser::parseFace(
                 b.color = faceColor;
                 this->finalVertices.push_back(b);
             }
-            auto currentVertex = this->parsedVertices[index - 1];
+            auto currentVertex = this->parsedVertices.at(index - 1);
             currentVertex.color = faceColor;
             this->finalVertices.push_back(currentVertex);
         }
@@ -186,17 +187,17 @@ void ObjParser::parseFaceTexture(
         const std::string& line) {
     try {
         for (size_t i = 1; i < lineSplit.size(); i++) {
-            size_t slashPos = lineSplit[i].find('/');
+            size_t slashPos = lineSplit.at(i).find('/');
             if (slashPos == std::string::npos) {
                 throw std::runtime_error("Inconsistent face format");
             }
-            std::vector<std::string> indices = utils::splitString(lineSplit[i], '/');
+            std::vector<std::string> indices = utils::splitString(lineSplit.at(i), '/');
             if (indices.size() != 2) {
                 throw std::runtime_error("Inconsistent face format");
             }
 
-            int vertexIndex = std::stoi(indices[0]);
-            int textCoordIndex = std::stoi(indices[1]);
+            int vertexIndex = std::stoi(indices.at(0));
+            int textCoordIndex = std::stoi(indices.at(1));
             // Todo: check for neg or overflow
             if (i == 4) {
                 auto a = *(this->finalVertices.rbegin() + 3 - 1);
@@ -206,9 +207,9 @@ void ObjParser::parseFaceTexture(
                 b.color = faceColor;
                 this->finalVertices.push_back(b);
             }
-            auto currentVertex = this->parsedVertices[vertexIndex - 1];
-            currentVertex.textX = this->textureCoords[textCoordIndex - 1].x;
-            currentVertex.textY = this->textureCoords[textCoordIndex - 1].y;
+            auto currentVertex = this->parsedVertices.at(vertexIndex - 1);
+            currentVertex.textX = this->textureCoords.at(textCoordIndex - 1).x;
+            currentVertex.textY = this->textureCoords.at(textCoordIndex - 1).y;
             currentVertex.color = faceColor;
             this->finalVertices.push_back(currentVertex);
         }
@@ -225,18 +226,18 @@ void ObjParser::parseFaceTextureNormal(
         const std::string& line) {
     try {
         for (size_t i = 1; i < lineSplit.size(); i++) {
-            size_t slashPos = lineSplit[i].find('/');
+            size_t slashPos = lineSplit.at(i).find('/');
             if (slashPos == std::string::npos) {
                 throw std::runtime_error("Inconsistent face format");
             }
-            std::vector<std::string> indices = utils::splitString(lineSplit[i], '/');
+            std::vector<std::string> indices = utils::splitString(lineSplit.at(i), '/');
             if (indices.size() != 3) {
                 throw std::runtime_error("Inconsistent face format");
             }
 
-            int vertexIndex = std::stoi(indices[0]);
-            int textCoordIndex = std::stoi(indices[1]);
-            int normalIndex = std::stoi(indices[2]);
+            int vertexIndex = std::stoi(indices.at(0));
+            int textCoordIndex = std::stoi(indices.at(1));
+            int normalIndex = std::stoi(indices.at(2));
             // Todo: check for neg or overflow
             if (i == 4) {
                 auto a = *(finalVertices.rbegin() + 3 - 1);
@@ -246,10 +247,10 @@ void ObjParser::parseFaceTextureNormal(
                 b.color = faceColor;
                 finalVertices.push_back(b);
             }
-            auto currentVertex = this->parsedVertices[vertexIndex - 1];
-            currentVertex.textX = this->textureCoords[textCoordIndex - 1].x;
-            currentVertex.textY = this->textureCoords[textCoordIndex - 1].y;
-            currentVertex.normal = this->normals[normalIndex - 1];
+            auto currentVertex = this->parsedVertices.at(vertexIndex - 1);
+            currentVertex.textX = this->textureCoords.at(textCoordIndex - 1).x;
+            currentVertex.textY = this->textureCoords.at(textCoordIndex - 1).y;
+            currentVertex.normal = this->normals.at(normalIndex - 1);
             currentVertex.color = faceColor;
             this->finalVertices.push_back(currentVertex);
         }
@@ -266,17 +267,17 @@ void ObjParser::parseFaceNormal(
         const std::string& line) {
     try {
         for (size_t i = 1; i < lineSplit.size(); i++) {
-            size_t slashPos = lineSplit[i].find('/');
+            size_t slashPos = lineSplit.at(i).find('/');
             if (slashPos == std::string::npos) {
                 throw std::runtime_error("Inconsistent face format");
             }
-            std::vector<std::string> indices = utils::splitString(lineSplit[i], '/');
-            if (indices.size() != 3 || (indices.size() == 3 && indices[1].size() != 0)) {
+            std::vector<std::string> indices = utils::splitString(lineSplit.at(i), '/');
+            if (indices.size() != 3 || (indices.size() == 3 && indices.at(1).size() != 0)) {
                 throw std::runtime_error("Inconsistent face format");
             }
 
-            int vertexIndex = std::stoi(indices[0]);
-            int normalIndex = std::stoi(indices[2]);
+            int vertexIndex = std::stoi(indices.at(0));
+            int normalIndex = std::stoi(indices.at(2));
             // Todo: check for neg or overflow
             if (i == 4) {
                 auto a = *(this->finalVertices.rbegin() + 3 - 1);
@@ -286,8 +287,8 @@ void ObjParser::parseFaceNormal(
                 b.color = faceColor;
                 this->finalVertices.push_back(b);
             }
-            auto currentVertex = this->parsedVertices[vertexIndex - 1];
-            currentVertex.normal = this->normals[normalIndex - 1];
+            auto currentVertex = this->parsedVertices.at(vertexIndex - 1);
+            currentVertex.normal = this->normals.at(normalIndex - 1);
             currentVertex.color = faceColor;
             this->finalVertices.push_back(currentVertex);
         }
@@ -302,23 +303,23 @@ void ObjParser::parseSmoothShading(const VecString& lineSplit) {
     if (lineSplit.size() != 2) {
         throw std::runtime_error("Incorrect smooth shading format: (1 / off)");
     }
-    if (lineSplit[1] == "1") {
+    if (lineSplit.at(1) == "1") {
         this->useSmoothShading = true;
-    } else if (lineSplit[1] == "off") {
+    } else if (lineSplit.at(1) == "off") {
         this->useSmoothShading = false;
     } else {
-        throw std::runtime_error("Incorrect smooth shading parameter: " + lineSplit[1]);
+        throw std::runtime_error("Incorrect smooth shading parameter: " + lineSplit.at(1));
     }
 }
 
 void ObjParser::determineFaceFormat(const std::vector<std::string>& lineSplit) {
-    VecString slashSplit = utils::splitString(lineSplit[1], '/');
+    VecString slashSplit = utils::splitString(lineSplit.at(1), '/');
     if (slashSplit.size() == 1) {
         this->faceFormat = VERTEX;
     } else if (slashSplit.size() == 2) {
         this->faceFormat = VERTEX_TEXTURE;
     } else if (slashSplit.size() == 3) {
-        if (slashSplit[1].size() == 0) {
+        if (slashSplit.at(1).size() == 0) {
             this->faceFormat = VERTEX_NORMAL;
         } else {
             this->faceFormat = VERTEX_TEXTURE_NORMAL;
